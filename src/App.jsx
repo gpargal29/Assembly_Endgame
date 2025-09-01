49a93833d0751b3e085176e8cedbd43c99b720fc
gpargal29 <pargalgunjan@gmail.com>
2025-09-01 16:38:21 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { languages } from "./languages.js";
+
+export default function AssemblyEndgame() {
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    return (
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
+  });
+
+  return (
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">{languageElements}</section>
+    </main>
+  );
+}
